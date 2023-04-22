22843444c4578eb4e80ec0848b6c40868b7deccb
Cinquin Andy <andy.cinquin@infotel.com>
2023-04-22 16:29:16 +0200

    🔥 supprimer(page.svelte) : supprimer le code de débogage console.log ✨ fonctionnalité(page.svelte) : ajouter une classe CSS pour améliorer la lisibilité du code ✨ fonctionnalité(page.svelte) : ajouter une balise LinkAuthenticationElement pour l'authentification ✨ fonctionnalité(page.svelte) : ajouter une balise PaymentElement pour le paiement ✨ fonctionnalité(page.svelte) : ajouter une balise Address pour l'adresse de facturation ✨ fonctionnalité(page.svelte) : ajouter un bouton de paiement stylisé ✨ fonctionnalité(page.svelte) : ajouter des informations supplémentaires sur le produit

--- src/routes/checkout/+page.svelte
+++ src/routes/checkout/+page.svelte
@@ -52,8 +52,6 @@
 			elements,
 			redirect: 'if_required',
 		})
-		// // log results, for debugging
-		// console.log({ result })
 		if (result.error) {
 			// payment failed, notify user
 			error = result.error
@@ -67,39 +65,39 @@
 
 <Header />
 
-<div class="relative flex min-h-[calc(100vh-100px)] w-screen items-center">
+<div class='relative flex min-h-[calc(100vh-100px)] w-screen items-center'>
 	<div
-		class="rotate-30 absolute left-0 top-0 -z-10 h-screen w-screen -translate-x-1/2 -translate-y-1/3 select-none opacity-5"
+		class='rotate-30 absolute left-0 top-0 -z-10 h-screen w-screen -translate-x-1/2 -translate-y-1/3 select-none opacity-5'
 	>
 		<svg
-			height="512"
-			viewBox="0 0 24 24"
-			width="512"
-			xmlns="http://www.w3.org/2000/svg"
-			class="h-full w-full fill-current"
+			height='512'
+			viewBox='0 0 24 24'
+			width='512'
+			xmlns='http://www.w3.org/2000/svg'
+			class='h-full w-full fill-current'
 		>
 			<path
-				d="m5 3a2 2 0 1 1 -2-2 2 2 0 0 1 2 2zm-2 7a1 1 0 1 0 -1-1 1 1 0 0 0 1 1zm0 6a1 1 0 1 0 -1-1 1 1 0 0 0 1 1zm0 3a2 2 0 1 0 2 2 2 2 0 0 0 -2-2zm6-18a2 2 0 1 0 2 2 2 2 0 0 0 -2-2zm1 8a1 1 0 1 0 -1 1 1 1 0 0 0 1-1zm-1 4a2 2 0 1 0 2 2 2 2 0 0 0 -2-2zm0 7a1 1 0 1 0 1 1 1 1 0 0 0 -1-1zm6-16a1 1 0 1 0 -1-1 1 1 0 0 0 1 1zm0 3a2 2 0 1 0 2 2 2 2 0 0 0 -2-2zm1 8a1 1 0 1 0 -1 1 1 1 0 0 0 1-1zm-1 4a2 2 0 1 0 2 2 2 2 0 0 0 -2-2zm6-14a2 2 0 1 0 -2-2 2 2 0 0 0 2 2zm-1 4a1 1 0 1 0 1-1 1 1 0 0 0 -1 1zm1 4a2 2 0 1 0 2 2 2 2 0 0 0 -2-2zm0 7a1 1 0 1 0 1 1 1 1 0 0 0 -1-1z"
+				d='m5 3a2 2 0 1 1 -2-2 2 2 0 0 1 2 2zm-2 7a1 1 0 1 0 -1-1 1 1 0 0 0 1 1zm0 6a1 1 0 1 0 -1-1 1 1 0 0 0 1 1zm0 3a2 2 0 1 0 2 2 2 2 0 0 0 -2-2zm6-18a2 2 0 1 0 2 2 2 2 0 0 0 -2-2zm1 8a1 1 0 1 0 -1 1 1 1 0 0 0 1-1zm-1 4a2 2 0 1 0 2 2 2 2 0 0 0 -2-2zm0 7a1 1 0 1 0 1 1 1 1 0 0 0 -1-1zm6-16a1 1 0 1 0 -1-1 1 1 0 0 0 1 1zm0 3a2 2 0 1 0 2 2 2 2 0 0 0 -2-2zm1 8a1 1 0 1 0 -1 1 1 1 0 0 0 1-1zm-1 4a2 2 0 1 0 2 2 2 2 0 0 0 -2-2zm6-14a2 2 0 1 0 -2-2 2 2 0 0 0 2 2zm-1 4a1 1 0 1 0 1-1 1 1 0 0 0 -1 1zm1 4a2 2 0 1 0 2 2 2 2 0 0 0 -2-2zm0 7a1 1 0 1 0 1 1 1 1 0 0 0 -1-1z'
 			/>
 		</svg>
 	</div>
 	<div
-		class="relative mx-auto flex flex-col gap-10 p-0 py-12 pt-36 sm:m-auto sm:grid sm:max-w-2xl sm:p-5 sm:py-12 sm:pt-36 lg:max-w-7xl lg:grid-cols-12 2xl:gap-10 2xl:pt-12"
+		class='relative mx-auto flex flex-col gap-10 p-0 py-12 pt-36 sm:m-auto sm:grid sm:max-w-2xl sm:p-5 sm:py-12 sm:pt-36 lg:max-w-7xl lg:grid-cols-12 2xl:gap-10 2xl:pt-12'
 	>
 		<div
-			class="relative col-span-12 mx-5 flex flex-col gap-20 sm:mx-0 sm:grid lg:grid-cols-12"
+			class='relative col-span-12 mx-5 flex flex-col gap-20 sm:mx-0 sm:grid lg:grid-cols-12'
 		>
 			<!-- Product image -->
-			<div class="col-span-6 lg:mt-0 lg:self-start">
-				<div class="relative mb-8 flex items-center justify-between">
+			<div class='col-span-6 lg:mt-0 lg:self-start'>
+				<div class='relative mb-8 flex items-center justify-between'>
 					<h1
-						class="tracking-loose text-3xl font-medium text-white sm:text-7xl"
+						class='tracking-loose text-3xl font-medium text-white sm:text-7xl'
 					>
 						{data.oeuvre.name}
 					</h1>
-					<h3 class="text-3xl">{data.oeuvre.price} €</h3>
+					<h3 class='text-3xl'>{data.oeuvre.price} €</h3>
 				</div>
-				<div class="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg">
+				<div class='aspect-w-1 aspect-h-1 overflow-hidden rounded-lg'>
 					<img
 						src={api_origin +
 							data.oeuvre.collectionId +
@@ -108,20 +106,19 @@
 							'/' +
 							data.oeuvre.image}
 						alt="image de l'oeuvre"
-						class="h-full w-full object-cover object-center"
+						class='h-full w-full object-cover object-center'
 					/>
 				</div>
-				<div class="mt-8 flex items-center justify-start" />
 			</div>
 
 			<!-- Product details -->
-			<div class="col-span-6 mt-0 flex flex-col justify-center gap-8 lg:mt-8">
-				<div class="flex w-full flex-col items-center justify-center">
+			<div class='col-span-6 mt-0 flex flex-col justify-center gap-8 lg:mt-8'>
+				<div class='flex w-full flex-col items-center justify-center'>
 					<div
-						class="w-full space-y-6 rounded-md bg-white px-4 py-6 shadow-md shadow-xl sm:px-6 lg:px-8"
+						class='w-full space-y-6 rounded-md bg-white px-4 py-6 shadow-md shadow-xl sm:px-6 lg:px-8'
 					>
 						{#if error}
-							<p class="error font-semibold text-gray-500">
+							<p class='error font-semibold text-gray-500'>
 								{error.message} Please try again.
 							</p>
 						{/if}
@@ -129,8 +126,8 @@
 							<Elements
 								{stripe}
 								{clientSecret}
-								theme="flat"
-								labels="floating"
+								theme='flat'
+								labels='floating'
 								variables={{ colorPrimary: '#4338CA' }}
 								rules={{ '.Input': { border: 'solid 1px #0002' } }}
 								bind:elements
@@ -138,12 +135,12 @@
 								<form on:submit|preventDefault={submit}>
 									<LinkAuthenticationElement />
 									<PaymentElement />
-									<Address mode="billing" />
+									<Address mode='billing' />
 
 									<!-- beatifull tailwind pay button -->
 									<button
 										disabled={processing}
-										class="my-4 w-full rounded bg-indigo-700 px-4 py-2 font-semibold text-white hover:bg-indigo-900"
+										class='my-4 w-full rounded bg-indigo-700 px-4 py-2 font-semibold text-white hover:bg-indigo-900'
 									>
 										{#if processing}
 											Chargement...
@@ -154,7 +151,7 @@
 								</form>
 							</Elements>
 						{:else}
-							<span class="font-semibold text-gray-500"> Loading... </span>
+							<span class='font-semibold text-gray-500'> Loading... </span>
 						{/if}
 					</div>
 				</div>
@@ -163,16 +160,16 @@
 
 		<!-- Product details -->
 		<div
-			class="col-span-12 mx-5 mt-0 flex flex-col justify-center gap-8 sm:mx-0"
+			class='col-span-12 mx-5 mt-0 flex flex-col justify-center gap-8 sm:mx-0'
 		>
 			<div>
-				<h3 class="text-3xl font-medium text-gray-50">
+				<h3 class='text-3xl font-medium text-gray-50'>
 					Informations supplémentaires
 				</h3>
-				<div class="prose prose-sm mt-4 text-gray-500">
-					<ul role="list" class="flex flex-col gap-2 pb-10 sm:pb-0">
+				<div class='prose prose-sm mt-4 text-gray-500'>
+					<ul role='list' class='flex flex-col gap-2 pb-10 sm:pb-0'>
 						{#each product_infos as product_info}
-							<li class="ml-4 text-sm">→ {product_info}</li>
+							<li class='ml-4 text-sm'>→ {product_info}</li>
 						{/each}
 					</ul>
 				</div>
